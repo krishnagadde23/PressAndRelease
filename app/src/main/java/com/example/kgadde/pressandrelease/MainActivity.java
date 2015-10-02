d85a0decba4092a7e5fca2764d2549472d4cbf1e
krishnagadde23 <krishnagadde23@gmail.com>
2015-10-02 11:58:09 -0400

    Day 6

--- app/src/main/java/com/example/kgadde/pressandrelease/MainActivity.java
+++ app/src/main/java/com/example/kgadde/pressandrelease/MainActivity.java
@@ -4,13 +4,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int press = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MyView v1 = (MyView) findViewById(R.id.t1);
+        v1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v1.incrementAndUpdate(press);
+                return false;
+            }
+        });
+
+        final MyView v2 = (MyView) findViewById(R.id.t2);
+        v2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v2.incrementAndUpdate(press);
+                return false;
+            }
+        });
+
+        final MyView v3 = (MyView) findViewById(R.id.t3);
+        v3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v3.incrementAndUpdate(press);
+                return false;
+            }
+        });
+
+        final MyView v4 = (MyView) findViewById(R.id.t4);
+        v4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v4.incrementAndUpdate(press);
+                return false;
+            }
+        });
     }
 
     @Override
