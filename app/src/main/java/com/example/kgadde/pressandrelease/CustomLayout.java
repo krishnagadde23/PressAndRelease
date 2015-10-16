614762d7bb2952369c525745b6bc14a2463ff7b5
krishnagadde23 <krishnagadde23@gmail.com>
2015-10-16 11:58:30 -0400

    Lab 07

--- app/src/main/java/com/example/kgadde/pressandrelease/CustomLayout.java
+++ app/src/main/java/com/example/kgadde/pressandrelease/CustomLayout.java
@@ -1,19 +1,59 @@
 package com.example.kgadde.pressandrelease;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 /**
  * Created by 2016kgadde on 10/5/2015.
  */
-public class CustomLayout extends RelativeLayout {
-    private MyView v1, v2, v3, v4;
-
-    public CustomLayout(Context context, AttributeSet attrs){
+public class CustomLayout extends RelativeLayout
+{
+    private View myUp;
+    public CustomLayout(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
-        LayoutInflater inflater = LayoutInflater.from(context);
+        myUp = null;
+    }
 
+    public View getUp()
+    {
+        return myUp;
+    }
+    public void setUp(View v)
+    {
+        myUp = v;
+    }
+
+    public boolean onInterceptTouchEvent(MotionEvent ev)
+    {
+        if(ev.getAction() == MotionEvent.ACTION_UP)
+        {
+            myUp = inViewBounds(ev);
+        }
+        return false;
+    }
+
+    public View inViewBounds(MotionEvent ev)
+    {
+        Rect viewRect = new Rect();
+        int[] loc = new int[2];
+        int rx = (int) ev.getRawX();
+        int ry = (int) ev.getRawY();
+        for(int i = 0; i < ((ViewGroup) this).getChildCount(); i++)
+        {
+            View child = ((ViewGroup) this).getChildAt(i);
+            child.getDrawingRect(viewRect);
+            child.getLocationOnScreen(loc);
+            viewRect.offset(loc[0], loc[1]);
+            if(viewRect.contains(rx, ry))
+                return child;
+        }
+        return null;
     }
 }
--- app/src/main/java/com/example/kgadde/pressandrelease/MainActivity.java
+++ app/src/main/java/com/example/kgadde/pressandrelease/MainActivity.java
@@ -1,123 +1,86 @@
 package com.example.kgadde.pressandrelease;
 
-import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private int press = 0;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+{
+    protected MyTextView[] textViews;
+    protected ViewGroup layout;
+    protected myOnTouchListener listener;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MyView v1 = (MyView) findViewById(R.id.t1);
-        final MyView v2 = (MyView) findViewById(R.id.t2);
-        final MyView v3 = (MyView) findViewById(R.id.t3);
-        final MyView v4 = (MyView) findViewById(R.id.t4);
 
-        v1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        textViews = new MyTextView[] {(MyTextView) findViewById(R.id.t1_textView), (MyTextView) findViewById(R.id.t2_textView),
+                (MyTextView) findViewById(R.id.t3_textView), (MyTextView) findViewById(R.id.t4_textView)};
+        layout = (ViewGroup) findViewById(R.id.layout);
+        listener = new myOnTouchListener();
 
-            }
-        });
-        v2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        v3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        v4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        v1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(inViewInBounds(v1, (int)event.getRawX(), (int)event.getRawY())) {
-                    v1.incrementAndUpdate(press);
-                }
-                if(inViewInBounds(v2, (int)event.getRawX(), (int)event.getRawY())) {
-                    v1.incrementAndUpdate(press);
-                }
-                if(inViewInBounds(v2, (int)event.getRawX(), (int)event.getRawY())) {
-                    v1.incrementAndUpdate(press);
-                }
-                if(inViewInBounds(v2, (int)event.getRawX(), (int)event.getRawY())) {
-                    v1.incrementAndUpdate(press);
-                }
-
-                return true;
-            }
-        });
-
-        v2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v2.incrementAndUpdate(press);
-                return true;
-            }
-        });
-
-        v3.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v3.incrementAndUpdate(press);
-                return true;
-            }
-        });
-
-        v4.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v4.incrementAndUpdate(press);
-                return true;
-            }
-        });
+        for(int x = 0; x < textViews.length; x++)
+        {
+            final int y = x;
+            textViews[x].setOnTouchListener(listener);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean inViewInBounds(View view, int x, int y){
-        Rect outRect = new Rect();
-        int[] location = new int[2];
-        view.getDrawingRect(outRect);
-        view.getLocationOnScreen(location);
-        outRect.offset(location[0], location[1]);
-        return outRect.contains(x, y);
+    class myOnTouchListener implements View.OnTouchListener
+    {
+        public boolean onTouch(View v, MotionEvent event)
+        {
+
+            if(event.getAction() == MotionEvent.ACTION_UP)
+            {
+                View up = (MyTextView) ((CustomLayout) layout).getUp();
+                if(up == null)
+                    return false;
+                int down = Arrays.asList(textViews).indexOf(v);
+                ((MyTextView) up).incrementAndUpdate(down);
+                return false;
+            }
+            return true;
+        }
     }
 }
